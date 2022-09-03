8bb01ab6aaeceffaa40857afe976f183ddddb027
Elessurgon <LancelotSycorax@gmail.com>
2022-09-03 23:07:24 +0530

    Add program independency

--- recon.ps1
+++ recon.ps1
@@ -1,4 +1,4 @@
-param ($domain, $dir = './', $name, $type = 'Passive')
+param ($domain, $dir = './', $name, $type = 'Passive', $chrome='C:\Program Files\Google\Chrome\Application\chrome.exe')
 if ($null -eq $name) {
     $name = $domain
 }
@@ -6,20 +6,26 @@ if ($null -eq $domain) {
     Write-host -Prompt "Enter domain" 
 }
 
-
 if (-not(Test-Path -Path $dir)) {
     New-Item -Path "$dir" -ItemType Directory
 }
-
 
 if ($type -eq 'Active') {
     ./amass.exe enum -active -v -src -ip -brute -min-for-recursive 2 -d $domain -o $dir/subdomains_all_$name.txt -p 80,443,8080
 } elseif ($type -eq 'Passive') {
     ./amass.exe enum -v -src -ip -brute -min-for-recursive 2 -d $domain -o $dir/subdomains_all_$name.txt
+} elseif($type -eq "Skip") {
+    Write-Host "Skipped scan"
 } else {
     Write-Host -Prompt "Write a valid option"
 }
-./amass.exe db -names -d $domain > $dir/subdomains_$name.txt
-./amass.exe viz -d3 -d $domain  
-Rename-Item -Path ".\amass.html" -NewName "$name.html"
-Move-Item -force -Path "./$name.html" -Destination $dir
+if ($type -ne "Skip") {
+    ./amass.exe db -names -d $domain > $dir/subdomains_$name.txt
+    ./amass.exe viz -d3 -d $domain  
+    Rename-Item -Path ".\amass.html" -NewName "$name.html"
+    Move-Item -force -Path "./$name.html" -Destination $dir
+}
+.\gowitness.exe file -f $dir\subdomains_$name.txt --chrome-path $chrome -F --delay 5 -P $dir/screenshots
+Move-Item -force -Path "./gowitness.sqlite3" -Destination $dir
+Set-Location $dir
+..\gowitness.exe server
